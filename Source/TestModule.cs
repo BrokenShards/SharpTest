8d2be90da64dfb35ff72b0f536590b5e7b9421a8
Shard White <breakingshards@gmail.com>
2020-11-15 23:10:24 +0000

    Version 0.1.0 Initial Upload

---
+++ Source/TestModule.cs
@@ -0,0 +1,101 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// TestModule.cs 
+////////////////////////////////////////////////////////////////////////////////
+//
+// SharpTest - A basic testing library for use with SFML.Net.
+// Copyright (C) 2020 Michael Furlong <michaeljfurlong@outlook.com>
+//
+// This program is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the Free 
+// Software Foundation, either version 3 of the License, or (at your option) 
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+// more details.
+// 
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using SFML.Graphics;
+
+namespace SharpTest
+{
+	/// <summary>
+	///   Base class for test modules.
+	/// </summary>
+	public abstract class TestModule
+	{
+		/// <summary>
+		///   Run module tests.
+		/// </summary>
+		/// <returns>
+		///   True if tests succeeded, otherwise false.
+		/// </returns>
+		public virtual bool RunTest( RenderWindow window = null )
+		{
+			return OnTest();
+		}
+
+		/// <summary>
+		///   Override this to run your module tests.
+		/// </summary>
+		/// <returns>
+		///   True if tests succeeded, otherwise false.
+		/// </returns>
+		protected abstract bool OnTest();
+	}
+	/// <summary>
+	///   Base class for test modules that also require a visual test.
+	/// </summary>
+	public abstract class VisualTestModule : TestModule
+	{
+		/// <summary>
+		///   Run module tests.
+		/// </summary>
+		/// <returns>
+		///   True if tests succeeded, otherwise false.
+		/// </returns>
+		public override bool RunTest( RenderWindow window )
+		{
+			return OnTest() && OnVisualTest( window );
+		}
+
+		/// <summary>
+		///   Run visual module tests.
+		/// </summary>
+		/// <param name="window">
+		///   The render window.
+		/// </param>
+		/// <returns>
+		///   True if tests succeeded, otherwise false.
+		/// </returns>
+		protected abstract bool OnVisualTest( RenderWindow window );
+	}
+
+	/// <summary>
+	///   Contains test related functionality.
+	/// </summary>
+	public static class Testing
+	{
+		/// <summary>
+		///   Runs a test module by type.
+		/// </summary>
+		/// <typeparam name="T">
+		///   The type of test module to run.
+		/// </typeparam>
+		/// <param name="window">
+		///   The render window.
+		/// </param>
+		/// <returns>
+		///   True if tests succeeded, otherwise false.
+		/// </returns>
+		public static bool Test<T>( RenderWindow window = null ) where T : TestModule, new()
+		{
+			return new T().RunTest( window );
+		}
+	}
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+﻿# SharpTest
+A basic testing library for use with SFML.Net.
+
+## Dependencies
+- SFML.Net `https://github.com/graphnode/SFML.Net.git`
+
+## Usage
+If your test does not require visual output, your class should should inherit from `TestModule` and
+testing should be done in `OnTest()`.<br>
+If your test does require visual output, your class should inherit from `VisualTestModule`, do
+non-visual testing in `OnTest()` and do visual testing in `OnVisualTest(RenderWindow)`.<br>
+Once your tests are written, they can be ran with `Testing.Test<TestType>(RenderWindow)`, leaving
+the render window null for non-visual tests.
+
+## Changelog
+
+### Version 0.1.0
+- Initial release.
